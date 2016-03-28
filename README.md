0cc539643d2e80d311e16f565b9ccfc29fd307f0
Andreas Lochwitz <andreas_lochwitz@live.de>
2016-03-28 21:03:56 +0200

    added source code, added explanation to README.md

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # map-script
 Load a leaflet map into your page by simply adding a div and script tag
+
+# Usage
+
+1. Add a div to your page:
+
+    ```
+    <div id="map" data-map-position="53.055072,8.783455" data-map-zoom="16" data-map-callback="mapCallback"></div>    
+    ```
+    
+    *Notes*
+    - id="map" is required
+    - data-map-position describes latitude and longitude of the position that the map should display
+    - data-map-zoom set the zoom-level of the map
+    - data-map-callback set a callback handler that will called after the map is loaded. The map-instance is given to the callback for further usage.
+
+2. Load the JavaScript file to load the map:
+
+    ```
+    <script src="map-script.js"></script>
+    ``` 
---
+++ src/map-script.js
@@ -0,0 +1,63 @@
+"use strict";
+
+(function() {
+    var loadState = {
+            'jsLoaded': false,
+            'cssLoaded': false
+        },
+        mapPosition = [53.055072, 8.783455],
+        mapZoom = 15,
+        mapCallback = function() { }; // dummy callback
+
+    // valid values for stateVar: 'cssLoaded' or 'jsLoaded'
+    function checkDone(stateVar) {
+        return function() {
+            loadState[stateVar] = true;
+            if (loadState.cssLoaded === true && loadState.jsLoaded === true) {
+                loadMap();
+            }
+        };
+    }
+
+    function loadMap() {
+        var map = L.map('map').setView(mapPosition, mapZoom);
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+        window[mapCallback](map);
+    }
+
+    function loadFile(fileUrl, loadCallback) {
+        var fileType = (fileUrl.split('.')).pop(),
+            tagElement = document.createElement(fileType === 'css' ? 'link' : 'script'),
+            headTag = null;
+        tagElement.onload = loadCallback;
+        if (fileType === 'css') {
+            tagElement.type = "text/css";
+            tagElement.rel = "stylesheet";
+            tagElement.href = fileUrl;
+            headTag = document.getElementsByTagName('head')[0];
+            headTag.appendChild(tagElement);
+        } else {
+            tagElement.src = fileUrl;
+            document.body.appendChild(tagElement);
+        }
+    }
+
+    function initData() {
+        var mapDiv = document.getElementById('map'),
+            data_map_position = mapDiv.getAttribute('data-map-position');
+        if (data_map_position != null) {
+            // remove whitespaces from data_map_position
+            data_map_position = data_map_position.replace(/ /g, '');
+            mapPosition = data_map_position.split(',');
+        }
+        mapCallback = mapDiv.getAttribute('data-map-callback');
+        mapZoom = parseInt(mapDiv.getAttribute('data-map-zoom'));
+    }
+
+    initData();
+    loadFile("http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.css", checkDone('cssLoaded'));
+    loadFile("http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js", checkDone('jsLoaded'));
+
+})();
