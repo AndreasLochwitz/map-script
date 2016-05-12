c551214ad3aac0bf8635cd4699b44444f0251740
Andreas Lochwitz <andreas_lochwitz@live.de>
2016-05-12 17:06:05 +0200

    Bug fixing - made parameters for zoom and callback optional, added 2 example files

--- src/map-script.js
+++ src/map-script.js
@@ -7,7 +7,7 @@
         },
         mapPosition = [53.055072, 8.783455],
         mapZoom = 15,
-        mapCallback = function() { }; // dummy callback
+        mapCallback = null; //function() { }; // dummy callback
 
     // valid values for stateVar: 'cssLoaded' or 'jsLoaded'
     function checkDone(stateVar) {
@@ -24,7 +24,9 @@
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
-        window[mapCallback](map);
+        if (mapCallback) {
+            window[mapCallback](map);
+        }
     }
 
     function loadFile(fileUrl, loadCallback) {
@@ -46,14 +48,20 @@
 
     function initData() {
         var mapDiv = document.getElementById('map'),
-            data_map_position = mapDiv.getAttribute('data-map-position');
+            data_map_position = mapDiv.getAttribute('data-map-position'),
+            t_mapZoom = parseInt(mapDiv.getAttribute('data-map-zoom')),
+            t_mapCallback = mapDiv.getAttribute('data-map-callback');
         if (data_map_position != null) {
             // remove whitespaces from data_map_position
             data_map_position = data_map_position.replace(/ /g, '');
             mapPosition = data_map_position.split(',');
         }
-        mapCallback = mapDiv.getAttribute('data-map-callback');
-        mapZoom = parseInt(mapDiv.getAttribute('data-map-zoom'));
+        if (t_mapCallback) {
+            mapCallback = t_mapCallback;
+        }
+        if (t_mapZoom) {
+            mapZoom = t_mapZoom;
+        }
     }
 
     initData();
